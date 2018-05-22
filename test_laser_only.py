757e59a3da8768478489bb2a741939d014fb92fa
Cloud <minhkjl3@gmail.com>
2018-05-22 19:45:45 +0700

    update test

--- test_laser_only.py
+++ test_laser_only.py
@@ -11,35 +11,41 @@ from laser_model import Qnet
 from experience_replay import ExperienceReplay
 from utils import Config
 
-env = gym.make('GazeboTurtlebotMazeColor-v0')
-qnet = Qnet(env.num_state, env.num_action)
+if(len(sys.argv) < 3):
+    print("Missing fields")
+    print("running command: python test_laser_only.py <from_pretrain_dir> <epsilon>")
 
-from_pretrain = sys.argv[1]
-
-if(from_pretrain != None):
-    qnet.load(from_pretrain)
 else:
-    print("Missing from pretrain field")
+    env = gym.make('GazeboTurtlebotMazeColor-v0')
+    qnet = Qnet(env.num_state, env.num_action)
 
-while True:
-    state = env.reset()
-    total_reward = 0
-    num_step = 0
-    for i in range(1000):
-        num_step += 1
-        # get action
-        action = qnet.get_actions(state.reshape(1, -1))[0]
+    from_pretrain = sys.argv[1]
+    epsilon = float(sys.argv[2])
+    qnet.load(from_pretrain)
 
-        # get after take action
-        newstate, reward, done, _ = env.step(action)
-        if(newstate == []):
-            print("Terminate")
-            # state = env.reset()
-            break
+    while True:
+        state = env.reset()
+        total_reward = 0
+        num_step = 0
+        num_random_step = 0
+        for i in range(1000):
+            num_step += 1
+            # get action
+            if(np.random.rand(1) < epsilon):
+                action = np.random.randint(env.num_action)
+                num_random_step += 1
+            else:
+                action = qnet.get_actions(state.reshape(1, -1))[0]
+            # get after take action
+            newstate, reward, done, _ = env.step(action)
+            if(newstate == []):
+                print("Terminate")
+                # state = env.reset()
+                break
 
-        total_reward += reward
-        state = newstate
-        if done:
-            break
+            total_reward += reward
+            state = newstate
+            if done:
+                break
 
-    print("\nDone episode in {} steps, Total reward: {}".format(num_step, total_reward))
+        print("\nDone episode in {} steps, {} random steps, Total reward: {}".format(num_step, num_random_step, total_reward))
